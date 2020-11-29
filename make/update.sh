b5d99d478d84db90e5aee7a32dce406b8e8dea3f
Akira Masuda <masuda.akira1014@gmail.com>
2020-11-29 17:01:20 +0900

    migrate to version_util

--- make/update.sh
+++ make/update.sh
@@ -1,32 +1,28 @@
 #!/usr/bin/env bash
 
-# update openjdk image
-current_jdk_version=$(awk '/ARG JDK_VERSION.*/ {
-    match($0, /"(.+)"/);
-    print substr($0, RSTART+1, RLENGTH-2);
-}' ./Dockerfile)
-next_jdk_version=$((current_jdk_version+1))
+set -eux
+
+source ./make/version_util.sh
+
+current_jdk_version=$(get_library_ver_dockerfile "JDK")
+next_jdk_version=$(increment_simple_version "${current_jdk_version}")
 ## check new image tag exists
 next_jdk_tag_url="https://registry.hub.docker.com/v2/repositories/openjdk/openjdk/tags/${next_jdk_version}-jdk-alpine"
-if curl -s -H "Accept: application/json" ${next_jdk_tag_url} | grep "errinfo" >/dev/null ; then
+if curl -s -H "Accept: application/json" "${next_jdk_tag_url}" | grep "errinfo" >/dev/null ; then
     next_jdk_version=${current_jdk_version}
 fi
 echo "openjdk : ${current_jdk_version} -> ${next_jdk_version}"
 
 # update plantuml
-current_plantuml_version=$(awk '/ARG PLANTUML_VERSION.*/ {
-    match($0, /"(.+)"/);
-    print substr($0, RSTART+1, RLENGTH-2);
-}' ./Dockerfile)
-next_plantuml_patch_version=$(echo "${current_plantuml_version}" | awk -F'.' '{
-    printf "%d.%d.%d",$1,$2,($3+1);
-}')
+current_plantuml_version=$(get_library_ver_dockerfile "PLANTUML")
+next_plantuml_patch_version=$(increment_semver_patch "${current_plantuml_version}")
 next_plantuml_patch_url="http://downloads.sourceforge.net/project/plantuml/${next_plantuml_patch_version}/plantuml.${next_plantuml_patch_version}.jar"
-if ! curl --head -s --fail ${next_plantuml_patch_url} ; then
+if ! curl --head -s --fail "${next_plantuml_patch_url}" ; then
     next_plantuml_patch_version=${current_plantuml_version}
 fi
 echo "plantuml : ${current_plantuml_version} -> ${next_plantuml_patch_version}"
 
+# replace to new version
 sed -E "s/ARG JDK_VERSION.*/ARG JDK_VERSION=\"${next_jdk_version}\"/" ./Dockerfile > ./Dockerfile.new && mv ./Dockerfile.new ./Dockerfile
 sed -E "s/ARG PLANTUML_VERSION.*/ARG PLANTUML_VERSION=\"${next_plantuml_patch_version}\"/" ./Dockerfile > ./Dockerfile.new && mv ./Dockerfile.new ./Dockerfile
 
